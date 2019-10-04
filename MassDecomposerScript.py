e73dc197e90b2d78fd190f625caf923f47d922e0
DeepSOIC <vv.titov@gmail.com>
2019-10-04 18:33:55 +0300

    Update molecules
    update N2O fragmentation from real data
    add isotopic CO2

--- MassDecomposerScript.py
+++ MassDecomposerScript.py
@@ -18,22 +18,29 @@ molecs = [
         'm31Int': 0.007188894,
     }),
     ('N2O', {
-        'm28Int': 0.000321429,
-        'm29Int': 0.012214286,
-        'm30Int': 0.123892857,
-        'm31Int': 0.149285714,
-        'm44Int': 0.001785714,
-        'm45Int': 0.067857143,
-        'm46Int': 0.644642857,
+        'm28Int': 0.150168492,
+        'm29Int': 0.049915754,
+        'm30Int': 0.125526537,
+        'm31Int': 0.119208088,
+        'm44Int': 0.01137321,
+        'm45Int': 0.055181129,
+        'm46Int': 0.621314238,
+        'm47Int': 0.017481045,
     }),
     ('N2', {
         'm28Int': 0.0025,
         'm29Int': 0.095,
         'm30Int': 0.9025,
     }),    
-    ('CO2', {
+    ('CO2nat', {
         'm44Int': 1.0,
     }),    
+    ('CO2iso', {
+        'm44Int': 0.359838915,
+        'm45Int': 0.51355651 ,
+        'm46Int': 0.004914336, #assuming low 18O due to T-induced isotopic exchange with ZnO
+        'm47Int': 0.007013664, #assuming low 18O due to T-induced isotopic exchange with ZnO
+    }),
 ]
 
 # Set weights for peaks. Higher weight mean more importance of the corresponding equation 
